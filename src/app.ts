67a55cedc27dca6c13ca2b2f8905f56568095d84
Gustavo Neris <nerisgs20@gmail.com>
2023-07-11 19:29:16 -0300

    feat: inicial commit

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import express, { NextFunction, Request, Response } from 'express'
+import { config } from 'dotenv'
+import cors from 'cors'
+import routes from './routes/routes'
+import { MongoClient } from './database/mongo'
+
+const app = express()
+
+app.use('/api', routes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message
+    })
+  }
+  res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err}`
+  })
+
+  next()
+})
+
+async function main() {
+  config()
+
+  app.use(cors())
+  app.use(express.json())
+
+  await MongoClient.connect()
+}
+
+main()
+
+export { app }
