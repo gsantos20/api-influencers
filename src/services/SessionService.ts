90afff1d7adfdada8c472b0081b5cbc197a0372d
Gustavo Neris <nerisgs20@gmail.com>
2023-07-14 00:48:43 -0300

    feat: add Model Login

--- src/services/SessionService.ts
+++ src/services/SessionService.ts
@@ -1,22 +1,18 @@
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
 import { IUsersRepository } from '@repositories/IUsersRepositories'
-
-type LoginRequest = {
-  username: string
-  password: string
-}
+import { Login } from '@models/Login'
 
 export class SessionService {
   constructor(private readonly userRepository: IUsersRepository) {}
-  async execute({ username, password }: LoginRequest) {
+  async execute({ username, password }: Login) {
     const user = await this.userRepository.findUser(username)
 
     if (!user) {
       return new Error('User does not exists!')
     }
 
-    const passwordMatch = await compare(password, user.password)
+    const passwordMatch = await compare(password, user.Password)
 
     if (!passwordMatch) {
       return new Error('User or Password incorrect')
