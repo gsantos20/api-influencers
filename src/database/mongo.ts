f5570af77a68f600198a86e0a71edd3008c0e0be
Gustavo Neris <nerisgs20@gmail.com>
2023-10-23 16:01:52 -0300

    feat: fix .env example and mongo connection

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,19 +1,13 @@
-import { MongoClient as Mongo, Db, AuthMechanism } from 'mongodb'
+import { AuthMechanism, Db, MongoClient as Mongo } from 'mongodb'
 
 export const MongoClient = {
   client: undefined as unknown as Mongo,
   db: undefined as unknown as Db,
 
   async connect(): Promise<void> {
-    const username = process.env.MONGODB_USERNAME
-    const password = process.env.MONGODB_PASSWORD
     const url = process.env.MONGODB_URL || ''
 
     const client = new Mongo(url, {
-      auth: {
-        username,
-        password
-      },
       authSource: 'dashboard-db',
       authMechanism: AuthMechanism.MONGODB_SCRAM_SHA1
     })
