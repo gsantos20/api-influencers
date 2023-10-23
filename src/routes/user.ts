62e7f45e75d40568f5073d43357c09308549fe3f
Gustavo Neris <nerisgs20@gmail.com>
2023-10-23 18:20:08 -0300

    feat: use old prisma

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,47 +1,54 @@
-import { CreateUserController } from '@controllers/CreateUserController'
-import { DeleteUserController } from '@controllers/DeleteUserController'
-import { GetUsersController } from '@controllers/GetUsersController'
-import { SessionController } from '@controllers/SessionController'
 import { ensuredAuthenticated } from '@middlewares/ensuredAuthenticated'
 import { methodNotAllowed } from '@middlewares/methodNotAllowed'
-import { PrismaUsersRepository } from '@repositories/prisma/PrismaUsersRepository'
-import { CreateUserService } from '@services/CreateUserService'
-import { DeleteUserService } from '@services/DeleteUserService'
-import { GetUsersService } from '@services/GetUsersService'
-import { SessionService } from '@services/SessionService'
 import express, { Router } from 'express'
+import { CreateUserController } from '@controllers/CreateUserController'
+import { CreateUserService } from '@services/CreateUserService'
+import { SessionService } from '@services/SessionService'
+import { SessionController } from '@controllers/SessionController'
+import { DeleteUserService } from '@services/DeleteUserService'
+import { DeleteUserController } from '@controllers/DeleteUserController'
+import { GetUsersService } from '@services/GetUsersService'
+import { GetUsersController } from '@controllers/GetUsersController'
+import { PrismaUsersRepository } from '@repositories/prisma/PrismaUsersRepository'
 
 const userRoutes: Router = express.Router()
+
 require('express-async-errors')
 
 const prsimaUsersRepository = new PrismaUsersRepository()
 
-userRoutes
-  .get('/users', ensuredAuthenticated(), async (req, res) => {
-    const getUsersService = new GetUsersService(prsimaUsersRepository)
-    const getUsersController = new GetUsersController(getUsersService)
+userRoutes.get('/users', ensuredAuthenticated(), async (req, res) => {
+  const getUsersService = new GetUsersService(prsimaUsersRepository)
+  const getUsersController = new GetUsersController(getUsersService)
 
-    getUsersController.handle(req, res)
-  })
-  .post('/user', async (req, res) => {
-    const createUserService = new CreateUserService(prsimaUsersRepository)
-    const createUserController = new CreateUserController(createUserService)
+  getUsersController.handle(req, res)
+})
 
-    return createUserController.handle(req, res)
-  })
-  .post('/login', async (req, res) => {
-    const sessionService = new SessionService(prsimaUsersRepository)
-    const sessionController = new SessionController(sessionService)
+userRoutes.post('/user', async (req, res) => {
+  const createUserService = new CreateUserService(prsimaUsersRepository)
+  const createUserController = new CreateUserController(createUserService)
 
-    return sessionController.handle(req, res)
-  })
-  .delete('/user/:id', ensuredAuthenticated(), async (req, res) => {
-    const deleteUserService = new DeleteUserService(prsimaUsersRepository)
-    const deleteUserController = new DeleteUserController(deleteUserService)
+  return createUserController.handle(req, res)
+})
 
-    return deleteUserController.handle(req, res)
-  })
+userRoutes.post('/login', async (req, res) => {
+  const sessionService = new SessionService(prsimaUsersRepository)
+  const sessionController = new SessionController(sessionService)
 
-userRoutes.all('/', methodNotAllowed)
+  return sessionController.handle(req, res)
+})
+
+userRoutes.delete('/user/:id', ensuredAuthenticated(), async (req, res) => {
+  const deleteUserService = new DeleteUserService(prsimaUsersRepository)
+  const deleteUserController = new DeleteUserController(deleteUserService)
+
+  return deleteUserController.handle(req, res)
+})
+
+userRoutes.all('/users', methodNotAllowed)
+
+userRoutes.all('/user', methodNotAllowed)
+
+userRoutes.all('/login', methodNotAllowed)
 
 export default userRoutes
